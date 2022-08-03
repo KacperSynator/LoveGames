c1c4f4cef29d0477ab914f97287602b8dfc1045f
KacperSynator <kacper.synator@gmail.com>
2022-08-03 18:12:54 +0200

    add ShootingGallery, update README.md

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # LoveGames
+
+Collection of simple 2D games created using [Love2D](https://love2d.org/) engine.
+
+## ShootingGallery
+
+Point and click game where the player shoots to randomly located targets. The goal is to shoot as many targets as possible until the time runs out. For every successful hit, score is increased by 1. Score is decreased by 1 for a miss.
+
+
+
+
+## Assets' sources
+
+* <a href='https://www.freepik.com/vectors/mountains-hill'>Mountains hill vector created by jcomp - www.freepik.com</a>  
+* <a href="https://www.flaticon.com/free-icons/crosshair" title="crosshair icons">Crosshair icons created by Freepik - Flaticon</a>  
+* <a href="https://www.flaticon.com/free-icons/target" title="target icons">Target icons created by Freepik - Flaticon</a>
---
+++ ShootingGallery/main.lua
@@ -0,0 +1,76 @@
+function love.load()
+    GamePaused = true
+
+    Target = {}
+    Target.x = 0
+    Target.y = 0
+    Target.radius = 64
+
+    MoveTargetToRandomPosition()
+
+    Score = 0
+    Timer = 0
+
+    Game_Font = love.graphics.newFont(40)
+
+    Sprites = {}
+    Sprites.background = love.graphics.newImage("Assets/Background_800x600.jpg")
+    Sprites.crosshair = love.graphics.newImage("Assets/crosshair.png")
+    Sprites.target = love.graphics.newImage("Assets/bullseye.png")
+
+    love.mouse.setVisible(false)
+end
+
+function love.update(dt)
+    if Timer > 0 and not GamePaused then
+        Timer = Timer - dt
+    end
+
+    if Timer < 0 then
+        Timer = 0
+        GamePaused = true
+    end
+end
+
+function love.draw()
+    love.graphics.draw(Sprites.background, 0, 0)
+
+    love.graphics.setColor(1, 1, 1)
+    love.graphics.setFont(Game_Font)
+    love.graphics.printf("Score: " .. Score, 5, 5, love.graphics.getWidth(), "left")
+    love.graphics.printf("Time: " .. math.ceil(Timer), -5, 5, love.graphics.getWidth(), "right")
+
+    if GamePaused then
+        love.graphics.printf("Click to begin!", 0, 250, love.graphics.getWidth(), "center")
+    end
+
+    if not GamePaused then
+        love.graphics.draw(Sprites.target, Target.x - Target.radius, Target.y - Target.radius)
+    end
+
+    love.graphics.draw(Sprites.crosshair, love.mouse.getX() - 16, love.mouse.getY() - 16)
+end
+
+function love.mousepressed(x, y, button, istouch, presses)
+    if button == 1 and not GamePaused then
+        if DistanceBetween(x, y, Target.x, Target.y) <= Target.radius then
+            Score = Score + 1
+            MoveTargetToRandomPosition()
+        else
+            Score = math.max(0, Score - 1)
+        end
+    elseif button == 1 and GamePaused then
+        GamePaused = false
+        Timer = 10
+        Score = 0
+    end
+end
+
+function DistanceBetween(x1, y1, x2, y2)
+    return math.sqrt((x2 - x1)^2 + (y2 - y1)^2)
+end
+
+function MoveTargetToRandomPosition()
+    Target.x = math.random(Target.radius, love.graphics.getWidth() - Target.radius)
+    Target.y = math.random(Target.radius, love.graphics.getHeight() - Target.radius)
+end
