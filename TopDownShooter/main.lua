335a1782449cd44e8eb66ce280e0fcb0c2291d61
KacperSynator <kacper.synator@gmail.com>
2022-08-06 11:09:13 +0200

    TopDownShooter: add player with basic movment

--- TopDownShooter/main.lua
+++ TopDownShooter/main.lua
@@ -1,8 +1,9 @@
 local anim8 = require "libraries/anim8/anim8"
 local map = require "map"
 local enemy = require "enemy"
+local player = require "player"
 
-local Map
+local Map, Player
 local Enemies = {}
 
 local MapScale = 1.2
@@ -12,12 +13,13 @@ function love.load()
 
     Map = map:new(nil, MapScale)
 
+    Player = player:new(nil, 100, 100)
+
     table.insert(Enemies, enemy:new(nil, 30, 30))
-    LoadSprites()
-    SetupAnimations()
 end
 
 function love.update(dt)
+    Player:move(dt)
     for _, en in pairs(Enemies) do
         en:move(dt)
     end
@@ -25,33 +27,8 @@ end
 
 function love.draw()
     Map:draw()
+    Player:draw()
     for _, en in pairs(Enemies) do
         en:draw()
     end
 end
-
-function LoadSprites()
-    Sprites = {}
-
-    Sprites.player_run_sheet = love.graphics.newImage("Assets/sPlayerRun_strip7.png")
-    Sprites.player_idle_sheet = love.graphics.newImage("Assets/sPlayerIdle_strip4.png")
-    Sprites.enemy_sheet = love.graphics.newImage("Assets/sEnemy_strip7.png")
-    Sprites.enemy_dead = love.graphics.newImage("Assets/sEnemyDead.png")
-    Sprites.bullet = love.graphics.newImage("Assets/sBullet.png")
-    Sprites.gun = love.graphics.newImage("Assets/sGun.png")
-end
-
-
-function SetupAnimations()
-    local player_run_grid = anim8.newGrid(40, 40, Sprites.player_run_sheet:getWidth(),
-                                                  Sprites.player_run_sheet:getHeight())
-    local player_idle_grid = anim8.newGrid(40, 40, Sprites.player_idle_sheet:getWidth(),
-                                                   Sprites.player_idle_sheet:getHeight())
-    local enemy_grid = anim8.newGrid(40, 40, Sprites.enemy_sheet:getWidth(),
-                                             Sprites.enemy_sheet:getHeight())
-    Animations = {}
-    Animations.player_run = anim8.newAnimation(player_run_grid('1-7', 1), 0.1)
-    Animations.player_idle = anim8.newAnimation(player_idle_grid('1-4', 1), 0.1)
-    Animations.enemy = anim8.newAnimation(enemy_grid('1-7', 1), 0.1)
-end
-
--- TopDownShooter/player.lua
+++ TopDownShooter/player.lua
@@ -1,1 +1,59 @@
-local player = {}
+local anim8 = require "libraries/anim8/anim8"
+local Enemy = require "enemy"
+
+
+function LoadAnimation(strip_path, frames, size_x, size_y, row, interval)
+    interval = interval or 0.1
+    local strip = love.graphics.newImage(strip_path)
+    local grid = anim8.newGrid(size_x, size_y, strip:getWidth(), strip:getHeight())
+    local result = {}
+    result.animation = anim8.newAnimation(grid(frames, row), interval)
+    result.image = strip
+    return result
+end
+
+local speed = 200
+local animation_idle = LoadAnimation("Assets/sPlayerIdle_strip4.png", '1-4', 40, 40, 1)
+local animation_run = LoadAnimation("Assets/sPlayerRun_strip7.png", '1-7', 40, 40, 1)
+
+local Player = Enemy:new()
+    function Player:new(o, x, y, rotation, scale_x, scale_y)
+        o = o or Enemy:new(o, x, y, rotation, scale_x, scale_y)
+        setmetatable(o, self)
+
+        self.__index = self
+        o.moving = false
+        o.speed = speed
+        o.image = animation_idle.image
+        o.animation = animation_idle.animation
+        return o
+    end
+
+    function Player:move(dt)
+        self.moving = true
+        
+        if love.keyboard.isDown("w") then
+            self.y = self.y - speed * dt
+        elseif love.keyboard.isDown("a") then
+            self.x = self.x - speed * dt
+        elseif love.keyboard.isDown("s") then
+            self.y = self.y + speed * dt
+        elseif love.keyboard.isDown("d") then
+            self.x = self.x + speed * dt
+        else
+            self.moving = false
+        end
+
+        if self.moving then
+            self.animation = animation_run.animation
+            self.image = animation_run.image
+        else
+            self.animation = animation_idle.animation
+            self.image = animation_idle.image
+        end
+
+        self.animation:update(dt)
+    end
+
+
+return Player
